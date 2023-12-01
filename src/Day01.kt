fb7c83631b818fe220e69b7653e718a9ad824b7a
tux <tux1986@googlemail.com>
2023-12-01 21:54:04 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,46 @@
 fun main() {
+    val digits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        .mapIndexed { i, s -> s to (i + 1) }
+        .toMap()
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.toCharArray().filter { it.isDigit() } }
+            .sumOf {
+                10 * (it.first().digitToInt()) + (it.last().digitToInt())
+            }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .sumOf { s ->
+
+                val digitNums =
+                    s.indices
+                        .filter  { s[it].isDigit() }
+                        .map     { s[it].digitToInt() to it }
+
+                val textNums = buildList {
+                    var idx: Int
+                    for (d in digits) {
+                        idx = s.indexOf(d.key)
+                        while (idx != -1) {
+                            add(d.value to idx)
+                            idx = s.indexOf(d.key, idx + 1)
+                        }
+                    }
+                }
+
+                val nums = (digitNums + textNums)
+                    .sortedBy { it.second }
+
+                10 * nums.first().first + nums.last().first
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()

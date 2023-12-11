fed6b864556537a8cb4fdd9c6fc7a4352c914243
tux <tux1986@googlemail.com>
2023-12-11 22:56:30 +0100

    Update Day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,71 +1,33 @@
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
-
-    val DX = intArrayOf(-1, 0, 1, 0)
-    val DY = intArrayOf( 0,-1, 0, 1)
-
     data class Point(val r: Int, val c: Int)
 
-    fun shortestPathPart(G: Array<CharArray>, s: Point, e: Point, emptyCols: Set<Int>, emptyRows: Set<Int>, factor: Int): Long {
-        val distance = Math.abs(e.r - s.r) + Math.abs(e.c - s.c)
+    fun shortestPathPart(s: Point, e: Point, emptyCols: Set<Int>, emptyRows: Set<Int>, factor: Int): Long {
+        val diff =
+            (min(s.r, e.r) ..< max(s.r, e.r)).count(emptyRows::contains) +
+            (min(s.c, e.c) ..< max(s.c, e.c)).count(emptyCols::contains)
 
-        var diff = 0L
-        for (r in Math.min(s.r, e.r) until Math.max(s.r, e.r)) {
-            if (emptyRows.contains(r)) {
-                diff++
-            }
-        }
-
-        for (c in Math.min(s.c, e.c) until Math.max(s.c, e.c)) {
-            if (emptyCols.contains(c)) {
-                diff++
-            }
-        }
-
-        return distance + (factor - 1) * diff
+        return abs(e.r - s.r) + abs(e.c - s.c) +
+                (factor - 1) * diff.toLong()
     }
 
     fun partHelp(I: List<String>, factor: Int): Long {
-        val emptyCols = mutableSetOf<Int>()
-        val emptyRows = mutableSetOf<Int>()
-
-        for (r in I.indices) {
-            val empty = I[r].count { it == '#' } == 0
-            if (empty) {
-                emptyRows.add(r)
+        val emptyCols = I[0].indices.filter { c -> I.all { it[c] != '#' } }.toSet()
+        val emptyRows = I.indices.filter { i -> I[i].none { it == '#' } }.toSet()
+        val galaxies = I.flatMapIndexed { r, row ->
+            row.mapIndexedNotNull { c, element ->
+                if (element == '#') Point(r, c) else null
             }
         }
 
-        for (c in 0 until I[0].length) {
-            var count = 0
-            for (element in I) {
-                if (element[c] == '#')
-                    count++
-            }
-            if (count == 0) {
-                emptyCols.add(c)
-            }
-        }
-
-        val G = Array(I.size) { I[it].toCharArray() }
-
-        val galaxies = mutableListOf<Point>()
-        for (r in G.indices) {
-            for (c in G[r].indices) {
-                if (G[r][c] == '#') {
-                    galaxies.add(Point(r, c))
-                }
-            }
-        }
-
-        var ret = 0L
-        for (i in 0 until galaxies.size) {
-            for (j in i + 1 until galaxies.size) {
-                val d = shortestPathPart(G, galaxies[i], galaxies[j], emptyCols, emptyRows, factor)
-                ret += d
-            }
-        }
-
-        return ret
+        return (galaxies.indices).mapIndexed { index, i ->
+            (i + 1 ..< galaxies.size).mapIndexed { index, j ->
+                shortestPathPart(galaxies[i], galaxies[j], emptyCols, emptyRows, factor)
+            }.sum()
+        }.sum()
     }
 
     fun part1(I: List<String>) = partHelp(I, 2)
